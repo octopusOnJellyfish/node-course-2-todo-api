1b361257221982376fcb709cf98a5a732139d8f7
Rodrigo <rodrigonasif@gmail.com>
2018-08-06 19:42:32 -0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,15 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+  return user.update({
+    $pull: {
+        tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -83,7 +92,7 @@ UserSchema.statics.findByCredentials = function (email, password) {
           resolve(user);
         } else {
           reject();
-        }        
+        }
       });
     });
   });
--- server/server.js
+++ server/server.js
@@ -120,7 +120,13 @@ app.post('/users/login', (req, res) => {
 
 
 // POST /users/login {email, password}
-
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
